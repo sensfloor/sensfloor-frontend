071263039224d69206ec2d0688962310d89560be
Julian Sehne <JulianM.sehne@gmail.com>
2026-02-06 21:38:17 +0100

    Add buffer

---
+++ src/buffer.js
@@ -0,0 +1,12 @@
+export function create_buffer() {
+  return {
+    _latest: null,
+    push: function (item) {
+      this._latest = item;
+      // console.log('Buffer updated with frame:', item.frame); // Optional debug
+    },
+    get: function () {
+      return this._latest;
+    },
+  };
+}
