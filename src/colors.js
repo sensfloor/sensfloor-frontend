d82a9c7e95942250c97709f681f3d93642f3ebf4
Julian Sehne <JulianM.sehne@gmail.com>
2026-02-06 21:08:41 +0100

    Add colors

---
+++ src/colors.js
@@ -0,0 +1,32 @@
+export const SKELETON_COLORS = [
+  // Skeleton 1: Red Theme
+  {
+    jointColor: 0xcc0000, // Dark Red
+    boneColor: 0xff5555   // Light Red
+  },
+  // Skeleton 2: Green Theme
+  {
+    jointColor: 0x00aa00, // Dark Green
+    boneColor: 0x66ff66   // Light Green
+  },
+  // Skeleton 3: Blue Theme
+  {
+    jointColor: 0x0000cc, // Dark Blue
+    boneColor: 0x5588ff   // Light Blue
+  },
+  // Skeleton 4: Yellow/Gold Theme
+  {
+    jointColor: 0xccaa00, // Dark Gold
+    boneColor: 0xffff66   // Light Yellow
+  },
+  // Skeleton 5: Purple Theme
+  {
+    jointColor: 0x880088, // Dark Purple
+    boneColor: 0xff66ff   // Light Purple
+  },
+  // Skeleton 6: Cyan/Teal Theme
+  {
+    jointColor: 0x008888, // Dark Teal
+    boneColor: 0x66ffff   // Light Cyan
+  }
+];
