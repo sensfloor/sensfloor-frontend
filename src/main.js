7f0390e3c17abbf598b79cfd53ee518cf51c20cd
felixbauer <bauerfe84318@th-nuernberg.de>
2026-02-07 12:44:19 +0100

    Remove white margin from body

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { animate_csv, setup_csv_animate } from "./animate_csv";
 import { animate_socket } from "./animate_socket";
 import { SKELETON_COLORS } from "./utils/colors";
+import './styles.css';
 
 const use_socket = true;
 
 const CSV_SKELETON_CONFIGS = [
-  { path: "../video_poses.csv", color: SKELETON_COLORS[5] , rotationOffset: 45},
+  { path: "../video_poses.csv", color: SKELETON_COLORS[5], rotationOffset: 45 },
   //{ path: "../predicted_poses_model_71.csv", color: SKELETON_COLORS[2] },
   //{path: "../landmark_weighted_feet_predictions.csv",  color: SKELETON_COLORS[2], rotationOffset: 0},
 ];
