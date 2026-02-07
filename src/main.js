4bebe18d30adada98a5c88c8be9e81667ac0ea21
Julian Sehne <JulianM.sehne@gmail.com>
2026-02-07 10:19:08 +0100

    runnable main

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ import { animate_csv, setup_csv_animate } from "./animate_csv";
 import { animate_socket } from "./animate_socket";
 import { SKELETON_COLORS } from "./utils/colors";
 
-const use_socket = true;
+const use_socket = false;
 
 const CSV_SKELETON_CONFIGS = [
   { path: "../video_poses.csv", color: SKELETON_COLORS[5] },
   //{ path: "../predicted_poses_model_71.csv", color: SKELETON_COLORS[2] },
-  {
-    path: "../landmark_weighted_feet_predictions.csv",
-    color: SKELETON_COLORS[0],
-  },
+  //{path: "../landmark_weighted_feet_predictions.csv",  color: SKELETON_COLORS[0],},
 ];
 
 if (use_socket) {
